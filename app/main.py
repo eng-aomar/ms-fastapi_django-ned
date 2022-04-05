74cce5bfefdaae0014ab46c9354f3cdff6510542
Alaa' Omar <alaa.omer2009@gmail.com>
2022-04-05 13:50:37 +0300

    commit

--- app/main.py
+++ app/main.py
@@ -1,21 +1,37 @@
-
-from urllib.request import Request
+import pathlib
+import os
+import io
+import uuid
+from functools import lru_cache
+from fastapi import(
+    FastAPI,
+    Header,
+    HTTPException,
+    Depends,
+    Request,
+    File,
+    UploadFile
+    )
+import pytesseract
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
-from fastapi import FastAPI, Request
-import pathlib
-app = FastAPI()
+from pydantic import BaseSettings
+from PIL import Image
+
+
 BASE_DIR = pathlib.Path(__file__).parent
 UPLOAD_DIR = BASE_DIR / "uploads"
 
+
+app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
 @app.get("/", response_class=HTMLResponse) # http GET -> JSON
-def home_view(request:Request ):
+def home_view(request: Request):
     return templates.TemplateResponse("home.html", {"request": request, "abc": 123})
 
 
-@app.post("/")
-def home_detail_view():
-    return {"Hello": "World"}
+
+
+
